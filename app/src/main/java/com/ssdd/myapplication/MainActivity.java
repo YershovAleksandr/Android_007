c5ed3b03d69d18bf6ce2c077575c238903a0a14b
hp <namstudionsk@gmail.com>
2017-11-27 23:08:59 +0700

    Add all labs files

--- app/src/main/java/com/ssdd/myapplication/MainActivity.java
+++ app/src/main/java/com/ssdd/myapplication/MainActivity.java
@@ -1,52 +1,82 @@
 package com.ssdd.myapplication;
 
+import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements
+		FriendsFragment.SelectionListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+	private static final String TAG = "Lab-Fragments";
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+	private FriendsFragment mFriendsFragment;
+	private FeedFragment mFeedFragment;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+		// If the layout is single-pane, create the FriendsFragment 
+		// and add it to the Activity
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+		if (!isInTwoPaneMode()) {
+			
+			mFriendsFragment = new FriendsFragment();
 
-        return super.onOptionsItemSelected(item);
-    }
+			//TODO 1 - add the FriendsFragment to the fragment_container
+			
+			
+			
+
+		} else {
+
+			// Otherwise, save a reference to the FeedFragment for later use
+
+			mFeedFragment = (FeedFragment) getFragmentManager()
+					.findFragmentById(R.id.feed_frag);
+		}
+
+	}
+
+	// If there is no fragment_container ID, then the application is in
+	// two-pane mode
+
+	private boolean isInTwoPaneMode() {
+
+		return findViewById(R.id.fragment_container) == null;
+	
+	}
+
+	// Display selected Twitter feed
+
+	public void onItemSelected(int position) {
+
+		Log.i(TAG, "Entered onItemSelected(" + position + ")");
+
+		// If there is no FeedFragment instance, then create one
+
+		if (mFeedFragment == null)
+			mFeedFragment = new FeedFragment();
+
+		// If in single-pane mode, replace single visible Fragment
+
+		if (!isInTwoPaneMode()) {
+
+			//TODO 2 - replace the fragment_container with the FeedFragment
+			
+
+			
+
+			// execute transaction now
+			getFragmentManager().executePendingTransactions();
+
+		}
+
+		// Update Twitter feed display on FriendFragment
+		mFeedFragment.updateFeedDisplay(position);
+
+	}
+
 }
